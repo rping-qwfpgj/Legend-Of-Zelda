d7153c83536e3046cb7d02c59eec773c6e28eaab
Vishal Kumar <kumar.853@buckeyemail.osu.edu>
2022-10-11 01:41:26 -0400

    CollissionDetector class

---
+++ Collision/CollisionDetector.cs
@@ -0,0 +1,81 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using static Sprint0.Link;
+using Interfaces;
+using Sprites;
+
+namespace Collision
+{
+	public class CollisionDetector
+	{
+
+		private Link currLink;
+		private List<ISprite> objects;
+		// private CollisionHandler handler;
+		private Room currRoom;
+		private List<ISprite> exclude;
+
+		public CollisionDetector(Link link, List<ISprite> sprites, Room room)
+		{
+			this.currLink = link;
+			// this.CollsionHandler handler = handler;
+			this.currRoom = room;
+			this.objects = room.ReturnObjects();
+			this.objects.add(currLink.getLinkSprite);
+			this.exclude = new List<ISprite>();
+
+		}
+
+		public boolean detectCollision(ISprite obj, ISprite otherObj)
+		{
+			Rectangle objectRec = obj.getRectangle();
+			Rectangle otherRec = otherObj.getRectangle();
+
+			return objectRec.Intersects(otherRec);
+		}
+
+		// method to determine type of collision ie. top-bottom or side-side collision to pass to handler
+		void determineSide(ISprite obj, ISprite otherObj)
+		{
+			
+		}
+
+
+		public void update()
+		{
+			// refresh objects array with the current rooms objects and add link in there
+			this.objects = currRoom.ReturnObjects();
+			objects.add(this.currLink);
+
+			
+			foreach (ISprite obj in this.objects)
+			{
+				foreach (ISprite otherObj in this.objects)
+				{
+					if (obj == otherObj)
+					{
+						continue;
+					}
+
+					if (!exclude.contains(obj)) { // only check for collision if object is not in our blacklist (there may be a better way to do this?)
+						if (detectCollision(obj, otherObj))
+						{
+							Console.WriteLine("collision detected!");
+							// pass some stuff and let the handler handle it from here
+						}
+					}
+				}
+				// now that we've checked all the possible collision interactins with this object, we don't need to agian for now
+				exclude.add(obj);
+			}
+
+
+		}
+
+
+
+
+
+	}
+}
