9acea8e3c479c3c300caf3344f92e497de54ef41
Lisha Nawani <nawani.4@buckeyemail.osu.edu>
2022-11-09 14:46:25 -0500

    separated backgrounds into individual files

---
+++ Backgrounds/Background4.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using LegendofZelda.Interfaces;
+
+namespace LegendofZelda.Backgrounds
+{
+    public class Background4 : ISprite
+    {
+        private readonly Texture2D texture;
+        private Rectangle sourceRectangle;
+        private Rectangle destinationRectangle;
+        public Rectangle DestinationRectangle { get => destinationRectangle; set => destinationRectangle = value; }
+        private readonly int width = 256;
+        private readonly int height = 176;
+
+        public Background4(Texture2D backgroundTexture)
+        {
+            texture = backgroundTexture;
+            sourceRectangle = new(515, 532, width, height);
+            destinationRectangle = new(0, 150, 800, 480);
+        }
+
+        public void Update()
+        {
+        }
+        public void Draw(SpriteBatch _spriteBatch)
+        {
+            _spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.None, RasterizerState.CullCounterClockwise);
+            _spriteBatch.Draw(texture, destinationRectangle, sourceRectangle, Color.White);
+            _spriteBatch.End();
+        }
+
+        public Rectangle GetHitbox()
+        {
+            return destinationRectangle;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
