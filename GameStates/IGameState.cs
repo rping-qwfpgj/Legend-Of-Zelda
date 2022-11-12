7c13880704f966164b9af758d0f1730af75489ac
Dylan Young <young.3113@osu.edu>
2022-11-12 16:09:53 -0500

    Implemented ILinkState

---
+++ GameStates/IGameState.cs
@@ -0,0 +1,14 @@
+﻿namespace Interfaces
+{
+    public interface IGameState
+    {
+        void ItemSelection();
+        void GameOver();
+        void Pause();
+        void WinGame();
+        void TransitionUp();
+        void TransitionDown();
+        void TransitionLeft();
+        void TransitionRight();        
+    }
+}
