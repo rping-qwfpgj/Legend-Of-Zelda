896ae0835679836bf5ec4ea93a45dd7494a5f0cb
Dylan Young <young.3113@osu.edu>
2022-11-12 16:31:04 -0500

    Made a controller class GameState, began implementing gameplaystate

---
+++ GameStates/GamePlayState.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Gamestates
+{   
+    public class GamePlayState : IGameState
+    {
+        public GamePlayState()
+        {
+            void ItemSelection()
+            {
+                
+            }
+            void GameOver()
+            {
+
+            }
+            void Pause();
+            void WinGame();
+            void TransitionUp();
+            void TransitionDown();
+            void TransitionLeft();
+            void TransitionRight();
+        }
+    }
+}
+
