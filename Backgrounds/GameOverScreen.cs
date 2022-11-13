801e26ec6f6ac761c162cb1c328a31e784e42077
Lisha Nawani <nawani.4@buckeyemail.osu.edu>
2022-11-13 15:32:33 -0500

    game over state

---
+++ Backgrounds/GameOverScreen.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using LegendofZelda.Interfaces;
+
+namespace LegendofZelda.Backgrounds
+{
+    public class GameOverScreen : ISprite
+    {
+        private readonly Texture2D texture;
+        private Rectangle sourceRectangle;
+        private Rectangle destinationRectangle;
+        public Rectangle DestinationRectangle { get => destinationRectangle; set => destinationRectangle = value; }
+        private readonly int width = 800;
+        private readonly int height = 480;
+
+
+        public GameOverScreen(Texture2D backgroundTexture)
+        {
+            texture = backgroundTexture;
+            sourceRectangle = new(0,0, 800, 480);
+            destinationRectangle = new(0, 150, 800, 480);
+        }
+
+        public void Update()
+        {
+        }
+        public void Draw(SpriteBatch _spriteBatch)
+        {
+            _spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.None, RasterizerState.CullCounterClockwise);
+            _spriteBatch.Draw(texture, destinationRectangle, sourceRectangle, Color.White);
+            _spriteBatch.End();
+        }
+
+        public Rectangle GetHitbox()
+        {
+            return destinationRectangle;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
