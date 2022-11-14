d1b4014f95685373e635657f30b61fe67e978033
Tuhin Patel <patel.4014@buckeyemail.osu.edu>
2022-11-13 20:03:40 -0800

    Bombable block stuff

--- Collision/LinkProjectileBlockHandler.cs
+++ Collision/LinkProjectileBlockHandler.cs
@@ -9,16 +9,22 @@ using LegendofZelda;
 using LegendofZelda.Interfaces;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
+using LegendofZelda.Blocks;
 
 namespace Collision
 {
     public static class LinkProjectileBlockHandler
 	{		
 
-		public static void handleCollision(ILinkProjectile projectile, Room room)
+		public static void handleCollision(ILinkProjectile projectile, IBlock block, Room room)
 		{
 			projectile.collide();
 			room.RemoveObject(projectile);
+
+			if(block is BombableDoorBlock)
+			{
+				room.RemoveObject(block);
+			}
 		}
 
 	}
