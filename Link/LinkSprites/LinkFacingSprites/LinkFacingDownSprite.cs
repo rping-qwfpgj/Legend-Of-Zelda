f6f0695f71ca521dddfd85f134a6a8d22a7772b4
Lisha Nawani <nawani.4@buckeyemail.osu.edu>
2022-11-21 15:40:22 -0500

    separated link sprites into individual files

---
+++ Link/LinkSprites/LinkFacingSprites/LinkFacingDownSprite.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Texture2D = Microsoft.Xna.Framework.Graphics.Texture2D;
+using Color = Microsoft.Xna.Framework.Color;
+using Rectangle = Microsoft.Xna.Framework.Rectangle;
+using LegendofZelda.Interfaces;
+
+namespace Sprites
+{
+    public class LinkFacingDownSprite : INonAttackingSprite
+    {
+        // Texture to take sprites from
+        private readonly Texture2D texture;
+
+        // X and Y positions of the sprite
+        private readonly int xPosition;
+        private readonly int yPosition;
+
+        private Color color = Color.White;
+        private bool isDamaged;
+
+        // Get information about screen dimensions
+        private Rectangle sourceRectangle;
+
+        // Screen location
+        private Rectangle destinationRectangle;
+        public Rectangle DestinationRectangle { get => new Rectangle(destinationRectangle.X-(destinationRectangle.Width/2), destinationRectangle.Y-(destinationRectangle.Height/2), destinationRectangle.Width, destinationRectangle.Height); set => destinationRectangle = value;}
+        public Vector2 Position { get => new(xPosition, yPosition); }
+        public LinkFacingDownSprite(Texture2D texture, float xPosition, float yPosition, bool isDamaged)
+        {
+            this.texture = texture;
+            xPosition = (int)xPosition;
+            yPosition = (int)yPosition;
+            this.isDamaged = isDamaged;
+
+            // Create source and destination rectangles
+            sourceRectangle = new Rectangle(1, 11, 15, 16); // Store the current location on the spritesheet to get a sprite from
+            destinationRectangle = new Rectangle((int)xPosition, (int)yPosition, sourceRectangle.Width*2, sourceRectangle.Height*2); // Where to draw on screen
+        }
+        public void Update()
+        {
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            // Draw the sprite
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.None, RasterizerState.CullCounterClockwise);
+            if (isDamaged)
+            {
+                spriteBatch.Draw(texture, destinationRectangle, sourceRectangle, Color.Lerp(Color.White, Color.Red, 0.3f), 0, new Vector2(sourceRectangle.Width / 2, sourceRectangle.Height / 2), SpriteEffects.None, 1);
+            } else
+            {
+                spriteBatch.Draw(texture, destinationRectangle, sourceRectangle, color, 0, new Vector2(sourceRectangle.Width / 2, sourceRectangle.Height / 2), SpriteEffects.None, 1);
+            }
+            spriteBatch.End();
+        }
+        public Rectangle GetHitbox()
+        {
+            return destinationRectangle;
+        }
+    }
+}
---
+++ Link/LinkSprites/LinkWalkingSprites/LinkWalkingRightSprite.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using LegendofZelda.Interfaces;
+
+namespace Sprites
+{
+    public class LinkWalkingRightSprite : INonAttackingSprite
+    {
+        // Keep track of frames
+        private int currFrames = 0;
+        private int maxFrames = 2000;
+
+        // Texture to take sprites from
+        private Texture2D texture;
+
+        // X and Y positions of the sprite
+        private int xPosition;
+        private int yPosition;
+        private bool isDamaged;
+
+        private Rectangle destinationRectangle;
+        private List<Rectangle> sourceRectangles;
+        private int currentFrameIndex;
+        public Rectangle DestinationRectangle
+        {
+            get => new Rectangle(destinationRectangle.X - (destinationRectangle.Width / 2), destinationRectangle.Y - (destinationRectangle.Height / 2), destinationRectangle.Width, destinationRectangle.Height); set => destinationRectangle = value;
+        }
+
+        public Vector2 Position { get => new(xPosition, yPosition); }
+        public LinkWalkingRightSprite(Texture2D texture, float xPosition, float yPosition, bool isDamaged)
+        {
+            this.texture = texture;
+            this.xPosition = (int)xPosition;
+            this.yPosition = (int)yPosition;
+            this.isDamaged = isDamaged;
+            sourceRectangles = new List<Rectangle>();
+            sourceRectangles.Add(new Rectangle(35, 11, 15, 16));
+            sourceRectangles.Add(new Rectangle(52, 12, 14, 15));
+            currentFrameIndex = 0;
+        }
+
+        public void Update()
+        {
+            // Update frames
+            currFrames += 100;
+
+            // If frames are past max, reset to 0
+            if (currFrames > maxFrames)
+            {
+                currFrames = 0;
+            }
+
+            this.xPosition += 2;
+         
+        }
+
+        // NOTE: All of these source Rectangles are using placeholder values for now
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+            // Create source and destination rectangles
+            if (currFrames >= 0 && currFrames <= maxFrames / 2)
+            {
+                currentFrameIndex = 0;
+            }
+            else if (currFrames > maxFrames / 2 && currFrames <= maxFrames) // Draw the 2nd step of link walking up
+            {
+                currentFrameIndex = 1;
+            }
+
+            Rectangle currentFrame = sourceRectangles[currentFrameIndex];
+            destinationRectangle = new Rectangle(xPosition, yPosition, currentFrame.Width * 2, currentFrame.Height * 2); // Where to draw on screen
+            
+
+            // Draw the sprite
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.None, RasterizerState.CullCounterClockwise);
+            if (isDamaged)
+            {
+                spriteBatch.Draw(texture, destinationRectangle, currentFrame, Color.Lerp(Color.White, Color.Red, 0.3f), 0, new Vector2(currentFrame.Width / 2, currentFrame.Height / 2), SpriteEffects.None, 1);
+            }
+            else
+            {
+                spriteBatch.Draw(texture, destinationRectangle, currentFrame, Color.White, 0, new Vector2(currentFrame.Width / 2, currentFrame.Height / 2), SpriteEffects.None, 1);
+            }
+            spriteBatch.End();
+        }
+
+        public Rectangle GetHitbox()
+        {
+
+            
+            return this.destinationRectangle;
+        }
+    }
+}
+
