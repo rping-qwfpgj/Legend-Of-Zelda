4f906ebeab9f9ee116f7a6d97a21a8466c1b390e
Lisha Nawani <nawani.4@buckeyemail.osu.edu>
2022-11-21 14:44:40 -0500

    .

--- Link/LinkCommands/LinkThrowingCommands.cs
+++ Link/LinkCommands/LinkThrowingCommands.cs
@@ -1,76 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using LegendofZelda.Interfaces;
+﻿using LegendofZelda.Interfaces;
 using Sprint0;
 
 namespace Commands
 {
-
     public class ThrowRightCommand : ICommand
     {
-        
-
         public ThrowRightCommand()
-        {
-            
+        { 
         }
-
         public void Execute()
         {
             Link.Instance.ThrowProjectile();
         }
-
     }
-
     public class ThrowLeftCommand : ICommand
     {
-        
-
         public ThrowLeftCommand()
         {
-            
         }
-
         public void Execute()
         {
             Link.Instance.ThrowProjectile();
         }
-
     }
-
     public class ThrowUpCommand : ICommand
     {
-        
-
         public ThrowUpCommand()
         {
-            
         }
-
         public void Execute()
         {
             Link.Instance.ThrowProjectile();
         }
-
     }
-
     public class ThrowDownCommand : ICommand
     {
-        
-
         public ThrowDownCommand()
         {
-            
         }
-
         public void Execute()
         {
             Link.Instance.ThrowProjectile();
         }
-
     }
 }
 
