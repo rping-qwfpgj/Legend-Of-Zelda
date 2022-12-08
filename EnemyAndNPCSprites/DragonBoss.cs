642fbee1630d5d9c55b547546eaa059e18ddc78c
jittaneybrin <20jin.brittaney@gmail.com>
2022-12-07 23:14:00 -0500

    separating out stuff

--- EnemyAndNPCSprites/DragonBoss.cs
+++ EnemyAndNPCSprites/DragonBoss.cs
@@ -3,10 +3,9 @@ using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using LegendofZelda.Interfaces;
-using Microsoft.Xna.Framework.Audio;
 using LegendofZelda.SpriteFactories;
-using System.Runtime.CompilerServices;
 using CommonReferences;
+using LegendofZelda.EnemyAndNPCSprites;
 
 namespace Sprites
 {
@@ -15,33 +14,18 @@ namespace Sprites
         private List<string> droppableItems = new List<string> { "Fairy" };
 
         // Keep track of frames
-        private int currFrames = 0;
-        private int maxFrames = 480;
-        private int deathFrames = 0;
-        private int maxDeathFrames = 40;
-
-        private int health = 3;
-        private bool isDamaged = false;
-        private int damagedCounter = 0;
-
-        // Texture to take sprites from
-        private Texture2D texture;
-        private Texture2D dyingTexture;
-
-        // X and Y positions of the sprite
-        private float xPosition;
-        private float yPosition;
+        private int currFrames = 0, maxFrames = 480, deathFrames = 0, maxDeathFrames = 40;
+        private int health = 3, damagedCounter =0;
+        private bool isDamaged = false, isDead = false, dyingComplete = false;
+        private Texture2D texture, dyingTexture;
+        private float xPosition, yPosition;
         private int direction = 1;
-        private bool isDead = false;
-        public bool IsDead { get => isDead; set => isDead = value; }
-        private bool dyingComplete = false;
-        public bool DyingComplete { get => dyingComplete; set => dyingComplete = value; }
 
-        // On screen location
-        private Rectangle dragonDestinationRectangle;
-        private Rectangle sourceRectangle;
+        private Rectangle sourceRectangle, dragonDestinationRectangle;
         private List<Rectangle> sourceRectangles;
 
+        public bool IsDead { get => isDead; set => isDead = value; }
+        public bool DyingComplete { get => dyingComplete; set => dyingComplete = value; }
         public Rectangle DestinationRectangle { get => dragonDestinationRectangle; set => dragonDestinationRectangle = value; }
 
         // Projectile orbs
@@ -66,7 +50,6 @@ namespace Sprites
             sourceRectangles.Add(new(51, 3, 9, 10));
             sourceRectangle = new();
         }
-
         public void Update()
         {
             if (!isDead)
@@ -103,7 +86,6 @@ namespace Sprites
                 deathFrames++;
             }
         }
-
         public void Draw(SpriteBatch spriteBatch)
         {
             Rectangle dragonSourceRectangle = new Rectangle(1, 11, 24, 32);
@@ -114,23 +96,18 @@ namespace Sprites
 
                 if (currFrames <= maxFrames / 8)
                 {
-                    // Dragon to draw
                     dragonSourceRectangle = new Rectangle(1, 11, 24, 32);
                 }
                 else if (currFrames <= maxFrames / 4)
                 {
-                    // Dragon rectangle
                     dragonSourceRectangle = new Rectangle(26, 11, 24, 32);
                 }
                 else if (currFrames <= maxFrames / 2)
                 {
-                    // Dragon rectangle
                     dragonSourceRectangle = new Rectangle(51, 11, 24, 32);
-
                 }
                 else if (currFrames <= maxFrames)
                 {
-                    // Dragon rectangle
                     dragonSourceRectangle = new Rectangle(76, 11, 24, 32);
                 }
                 spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.None, RasterizerState.CullCounterClockwise);
@@ -170,18 +147,13 @@ namespace Sprites
             }
 
         }
-
         public Rectangle GetHitbox()
         {
-            // TEMPORARY, working on what to put here
             return dragonDestinationRectangle;
         }
 
-        public void TurnAround(string side)
-        {
-            //dragon can't turn around
-        }
-
+        public void TurnAround(string side) { }
+   
         // Allows other things in the game to get the projectiles
         public List<ISprite> getEnemyProjectiles()
         {
@@ -215,289 +187,10 @@ namespace Sprites
             }
         }
 
-        public void PoofIn(SpriteBatch spriteBatch) { }
-
-    }
-
-    public class TopDragonAttackOrbSprite : IEnemyProjectile
-    {
-        // Keep track of frames
-        private int currFrames = 0;
-        private int maxFrames = 10000;
-
-        // Texture to take sprites from
-        private Texture2D texture;
-
-        // X and Y positions of the sprite
-        private int xPosition;
-        private int yPosition;
-
-        // Original positions to reset to
-        private int originalX;
-        private int originalY;
-
-        public bool keepThrowing { get; set; }
-
-        // Orbs will rapidly swap between 4 different version
-        private List<Rectangle> attackOrbs = new List<Rectangle>();
-        private Rectangle blueOrb = new Rectangle(128, 14, 8, 10);
-        private Rectangle orangeOrb = new Rectangle(119, 14, 8, 10);
-        private Rectangle greenOrb = new Rectangle(110, 14, 8, 10);
-        private Rectangle multicolorOrb = new Rectangle(101, 14, 8, 10);
-        private int currOrb; // Represents which orb from the list to draw
-
-        // On Screen location
-        private Rectangle destinationRectangle;
-        public Rectangle DestinationRectangle { get => destinationRectangle; set => destinationRectangle = value; }
-
-        public TopDragonAttackOrbSprite(Texture2D texture, float xPosition, float yPosition)
+        public void PoofIn(SpriteBatch spriteBatch)
         {
-            this.texture = texture;
-            this.xPosition = (int)xPosition;
-            this.yPosition = (int)yPosition;
-            originalX = (int)xPosition;
-            originalY = (int)yPosition;
-            destinationRectangle = new Rectangle((int)xPosition, (int)yPosition, 32, 40);
-
-            currOrb = 0;
-            attackOrbs.Add(blueOrb);
-            attackOrbs.Add(orangeOrb);
-            attackOrbs.Add(greenOrb);
-            attackOrbs.Add(multicolorOrb);
-            keepThrowing = true;
+           
         }
-        public void Update()
-        {
-            // Update current orb
-            currFrames += 1;
-            if (currFrames % 2 == 0)
-            {
-                ++currOrb;
-            }
-            if (currOrb >= attackOrbs.Count)
-            {
-                currOrb = 0;
-            }
-
-            //update location
-            if (currFrames % 3 == 0)
-                yPosition -= 5;
-            xPosition -= 5;
-
-            // Update the full location of the orb
-            destinationRectangle = new((int)xPosition, (int)yPosition, 32, 40);
-
-        }
-
-        public void Draw(SpriteBatch spriteBatch)
-        {
-
-            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.None, RasterizerState.CullCounterClockwise);
-            spriteBatch.Draw(texture, destinationRectangle, attackOrbs[currOrb], Color.White);
-            spriteBatch.End();
-
-        }
-
-        public Rectangle GetHitbox()
-        {
-            return destinationRectangle;
-
-        }
-
-        public void collide()
-        {
-            currFrames = maxFrames;
-            keepThrowing = false;
-
-        }
-
-    }
-
-    public class MiddleDragonAttackOrbSprite : IEnemyProjectile
-    {
-        // Keep track of frames
-        private int currFrames = 0;
-        private int maxFrames = 10000;
-
-        // Texture to take sprites from
-        private Texture2D texture;
-
-        // X and Y positions of the sprite
-        private int xPosition;
-        private int yPosition;
-
-        // Original positions to reset to
-        private int originalX;
-        private int originalY;
-
-        // Keeps track of if the projectile should keep going
-        public bool keepThrowing { get; set; }
-
-        // Orbs will rapidly swap between 4 different version
-        private List<Rectangle> attackOrbs = new List<Rectangle>();
-        private Rectangle blueOrb = new Rectangle(128, 14, 8, 10);
-        private Rectangle orangeOrb = new Rectangle(119, 14, 8, 10);
-        private Rectangle greenOrb = new Rectangle(110, 14, 8, 10);
-        private Rectangle multicolorOrb = new Rectangle(101, 14, 8, 10);
-        private int currOrb; // Represents which orb from the list to draw
-
-        // On Screen location
-        private Rectangle destinationRectangle;
-        public Rectangle DestinationRectangle { get => destinationRectangle; set => destinationRectangle = value; }
-
-        public MiddleDragonAttackOrbSprite(Texture2D texture, float xPosition, float yPosition)
-        {
-            this.texture = texture;
-            this.xPosition = (int)xPosition;
-            this.yPosition = (int)yPosition;
-            originalX = (int)xPosition;
-            originalY = (int)yPosition;
-            destinationRectangle = new Rectangle((int)xPosition, (int)yPosition, 32, 40);
-
-            currOrb = 0;
-            attackOrbs.Add(blueOrb);
-            attackOrbs.Add(orangeOrb);
-            attackOrbs.Add(greenOrb);
-            attackOrbs.Add(multicolorOrb);
-            keepThrowing = true;
-        }
-        public void Update()
-        {
-
-            // Update current orb
-            currFrames += 1;
-            if (currFrames % 2 == 0)
-            {
-                ++currOrb;
-            }
-            if (currOrb >= attackOrbs.Count)
-            {
-                currOrb = 0;
-            }
-
-            //update position
-            xPosition -= 5;
-
-            // Update the full location of the orb
-            destinationRectangle = new Rectangle((int)xPosition, (int)yPosition, 32, 40);
-
-        }
-
-        public void Draw(SpriteBatch spriteBatch)
-        {
-            //if(currFrames < maxFrames) {
-            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.None, RasterizerState.CullCounterClockwise);
-            spriteBatch.Draw(texture, destinationRectangle, attackOrbs[currOrb], Color.White);
-            spriteBatch.End();
-            //} 
-        }
-
-        public Rectangle GetHitbox()
-        {
-            return destinationRectangle;
-
-        }
-        public void collide()
-        {
-            currFrames = maxFrames;
-            keepThrowing = false;
-        }
-
-    }
-
-    public class BottomDragonAttackOrbSprite : IEnemyProjectile
-    {
-        // Keep track of frames
-        private int currFrames = 0;
-        private int maxFrames = 10000;
-
-        // Texture to take sprites from
-        private Texture2D texture;
-
-        // X and Y positions of the sprite
-        private int xPosition;
-        private int yPosition;
-
-        // Original positions to reset to
-        private int originalX;
-        private int originalY;
-
-        // Keep track of if the projectile should keep going
-        public bool keepThrowing { get; set; }
-
-
-        // Orbs will rapidly swap between 4 different version
-        private List<Rectangle> attackOrbs = new();
-        private Rectangle blueOrb = new(128, 14, 8, 10);
-        private Rectangle orangeOrb = new(119, 14, 8, 10);
-        private Rectangle greenOrb = new(110, 14, 8, 10);
-        private Rectangle multicolorOrb = new(101, 14, 8, 10);
-        private int currOrb; // Represents which orb from the list to draw
-
-        // On Screen location
-        private Rectangle destinationRectangle;
-        public Rectangle DestinationRectangle { get => destinationRectangle; set => destinationRectangle = value; }
-
-        public BottomDragonAttackOrbSprite(Texture2D texture, float xPosition, float yPosition)
-        {
-            this.texture = texture;
-            this.xPosition = (int)xPosition;
-            this.yPosition = (int)yPosition;
-            originalX = (int)xPosition;
-            originalY = (int)yPosition;
-            destinationRectangle = new Rectangle((int)xPosition, (int)yPosition, 32, 40);
-
-            currOrb = 0;
-            attackOrbs.Add(blueOrb);
-            attackOrbs.Add(orangeOrb);
-            attackOrbs.Add(greenOrb);
-            attackOrbs.Add(multicolorOrb);
-            keepThrowing = true;
-        }
-        public void Update()
-        {
-
-            currFrames += 1;
-            // Update current orb
-            if (currFrames % 2 == 0)
-            {
-                ++currOrb;
-            }
-            if (currOrb >= attackOrbs.Count)
-            {
-                currOrb = 0;
-            }
-
-            // Update x and y so that this orb goes towards the upper left in a diagonal line
-            if (currFrames % 3 == 0)
-                yPosition += 5;
-            xPosition -= 5;
-
-
-            // Update the full location of the orb
-            destinationRectangle = new Rectangle((int)xPosition, (int)yPosition, 32, 40);
-
-        }
-
-        public void Draw(SpriteBatch spriteBatch)
-        {
-
-            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.None, RasterizerState.CullCounterClockwise);
-            spriteBatch.Draw(texture, destinationRectangle, attackOrbs[currOrb], Color.White);
-            spriteBatch.End();
-
-        }
-
-        public Rectangle GetHitbox()
-        {
-            return destinationRectangle;
-
-        }
-        public void collide()
-        {
-            currFrames = maxFrames;
-            keepThrowing = false;
-        }
-    }
+    } 
 }
 
---
+++ EnemyAndNPCSprites/DragonBottomOrb.cs
@@ -0,0 +1,106 @@
+﻿using LegendofZelda.Interfaces;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegendofZelda.EnemyAndNPCSprites
+{
+    public class BottomDragonAttackOrbSprite : IEnemyProjectile
+    {
+        // Keep track of frames
+        private int currFrames = 0;
+        private int maxFrames = 10000;
+
+        // Texture to take sprites from
+        private Texture2D texture;
+
+        // X and Y positions of the sprite
+        private int xPosition;
+        private int yPosition;
+
+        // Original positions to reset to
+        private int originalX;
+        private int originalY;
+
+        // Keep track of if the projectile should keep going
+        public bool keepThrowing { get; set; }
+
+
+        // Orbs will rapidly swap between 4 different version
+        private List<Rectangle> attackOrbs = new();
+        private Rectangle blueOrb = new(128, 14, 8, 10);
+        private Rectangle orangeOrb = new(119, 14, 8, 10);
+        private Rectangle greenOrb = new(110, 14, 8, 10);
+        private Rectangle multicolorOrb = new(101, 14, 8, 10);
+        private int currOrb; // Represents which orb from the list to draw
+
+        // On Screen location
+        private Rectangle destinationRectangle;
+        public Rectangle DestinationRectangle { get => destinationRectangle; set => destinationRectangle = value; }
+
+        public BottomDragonAttackOrbSprite(Texture2D texture, float xPosition, float yPosition)
+        {
+            this.texture = texture;
+            this.xPosition = (int)xPosition;
+            this.yPosition = (int)yPosition;
+            originalX = (int)xPosition;
+            originalY = (int)yPosition;
+            destinationRectangle = new Rectangle((int)xPosition, (int)yPosition, 32, 40);
+
+            currOrb = 0;
+            attackOrbs.Add(blueOrb);
+            attackOrbs.Add(orangeOrb);
+            attackOrbs.Add(greenOrb);
+            attackOrbs.Add(multicolorOrb);
+            keepThrowing = true;
+        }
+        public void Update()
+        {
+
+            currFrames += 1;
+            // Update current orb
+            if (currFrames % 2 == 0)
+            {
+                ++currOrb;
+            }
+            if (currOrb >= attackOrbs.Count)
+            {
+                currOrb = 0;
+            }
+
+            // Update x and y so that this orb goes towards the upper left in a diagonal line
+            if (currFrames % 3 == 0)
+                yPosition += 5;
+            xPosition -= 5;
+
+
+            // Update the full location of the orb
+            destinationRectangle = new Rectangle((int)xPosition, (int)yPosition, 32, 40);
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.None, RasterizerState.CullCounterClockwise);
+            spriteBatch.Draw(texture, destinationRectangle, attackOrbs[currOrb], Color.White);
+            spriteBatch.End();
+
+        }
+
+        public Rectangle GetHitbox()
+        {
+            return destinationRectangle;
+
+        }
+        public void collide()
+        {
+            currFrames = maxFrames;
+            keepThrowing = false;
+        }
+    }
+}
